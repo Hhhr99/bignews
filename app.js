85d19a8882c2c8c8f73ded706b5417321ead1ac1
Hhhr <Hhhr1214@gmail.com>
2021-03-31 10:16:43 +0800

    设置uploads为静态资源目录

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const server = express()
 const cors = require('cors')
 server.use(cors())
 
-
+// 静态资源托管
+server.use('/uploads',express.static('uploads'))
 
 server.listen(3001, () => {
     console.log('3001监听就绪')
