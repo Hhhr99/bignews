926d9c9951b0a13e8ea9a5f84da30e945ae81bac
Hhhr <Hhhr1214@gmail.com>
2021-03-31 10:21:13 +0800

    设置express-jwt第三方包用于token生成和验证

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const cors = require('cors')
 server.use(cors())
 
 // 静态资源托管
-server.use('/uploads',express.static('uploads'))
+server.use('/uploads', express.static('uploads'))
+
+// jwt生成token
+const jwt = require('express-jwt')
+// app.use(jwt().unless());
+// jwt() 用于解析token，并将 token 中保存的数据 赋值给 req.user
+// unless() 约定某个接口不需要身份认证
+server.use(jwt({
+    secret: 'bignews',      // 生成token时的 钥匙，必须统一
+    algorithms: ['HS256']       // 必填，加密算法，无需了解
+}).unless({
+    path: ['/user/login', '/user/register', /^\/uploads\/.*/] // 除了这两个接口，其他都需要认证
+}));
+
 
 server.listen(3001, () => {
     console.log('3001监听就绪')
